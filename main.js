21838baae9720e0237aa3c0b1e6af05bee5cbccd
Asha <asha.vakkattil@gmail.com>
2020-12-23 23:35:58 +0530

    Removed js code from index.html and added it to main.js file

---
+++ main.js
@@ -0,0 +1,11 @@
+//Mobile menu
+var mobile = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(mobile, {});
+
+//Parallax init
+var parallax = document.querySelectorAll('.parallax');
+var instances = M.Parallax.init(parallax, {});
+
+//Scrollspy init
+var ss = document.querySelectorAll('.scrollspy')
+var instances = M.ScrollSpy.init(ss, {})
